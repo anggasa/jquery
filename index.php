219a2202a4ad7ceeebff419af89d8c2f1366c26b
anggasa <anqghasiyaputra@hotmail.com>
2017-03-10 16:24:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,192 @@
+<html lang="en">
+<head>
+</head>
+<body>&nbsp;<title>Free Premium SSH Account</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="/js/request.js"></script>
+  <style>
+.center_div{
+    margin: 0 auto;
+    width:80% /* value of your choice which suits your alignment */
+}
+.navbar { margin:0 auto;}
+p.small {
+    line-height: 0.3;
+//  overflow-y: scroll;
+}
+	#bs-example{
+    	margin: 15px;
+    }
+
+    /* Remove the navbar's default rounded borders and increase the bottom margi
+
+    /* Remove the jumbotron's default bottom margin */ 
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+      margin-bottom:50px;
+    }
+.server{
+	font-family: arial, helvetica, sans-serif;
+//	float:left;
+	width:200px;
+	margin:0px;
+	height:120px;
+}
+#myForm{
+        font-family: arial, helvetica, sans-serif;
+        float:center;
+        width: 480px;
+        background:#fff;
+        margin:15px;
+        border:1px solid #ccc;
+        height:260px;
+        position:relative;
+}
+#serverSE{
+	font-family: arial, helvetica, sans-serif;
+	float:left;
+	width:390px;
+	background:#fff;
+	margin:15px;
+	border:1px solid #ccc;
+	height:160px;
+	position:relative;
+}
+#server td,#server th,#serverSE td,#serverSE th{
+	text-align:left;
+	padding:2px 4px;
+}
+#server-location{
+	border-bottom:3px solid #FF9900;
+	background:#15242D;
+	padding:8px 8px;
+	font-size:18px;
+	color:#fff;
+	line-height:25px;
+	font-family: Trebuchet MS, Arial, Verdana, Geneva, Helvetica, sans-serif;
+	text-shadow:1px 0px 0px #000;
+}
+#server-account{
+	font-size:small;
+	font-family: Arial;
+	text-align:center;
+	position:absolute;
+	bottom:0;
+	right:0;
+	left:0;
+}
+#server-account a{
+	border:1px solid #ccc;-webkit-box-shadow: #FEFFFF 0px 1px 1px ;-moz-box-shadow: #FEFFFF 0px 1px 1px ; box-shadow: #FEFFFF 0px 1px 1px ; -webkit-border-radius: 3px; -moz-border-radius: 3px;border-radius: 3px;font-size:12px;font-family:arial, helvetica, sans-serif; text-shadow: 0px 1px 0 rgba(255,255,255,1);font-weight:bold; text-align: center; color: #4A4A4A; background-color: #F7F5F6;
+	background-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, #F7F5F6), color-stop(100%, #DDDDDD));
+	background-image: -webkit-linear-gradient(top, #F7F5F6, #DDDDDD);
+	background-image: -moz-linear-gradient(top, #F7F5F6, #DDDDDD);
+	background-image: -ms-linear-gradient(top, #F7F5F6, #DDDDDD);
+	background-image: -o-linear-gradient(top, #F7F5F6, #DDDDDD);
+	background-image: linear-gradient(top, #F7F5F6, #DDDDDD);filter:progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr=#F7F5F6, endColorstr=#DDDDDD);
+	color:#000;
+	padding:10px;
+}
+#server-account a:hover,#server:hover{
+	box-shadow:0 0 3px #CCC;
+}
+#server-detail{
+	padding:2px 10px;
+	/*font: 100%/1.1 "Trebuchet MS", sans-serif;*/
+}
+  </style>
+
+
+
+<nav class="navbar navbar-inverse bg-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/"><span class="glyphicon glyphicon-home"></span></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">SSH</a></li>
+        <li><a href="#">Vpn</a></li>
+        <li><a href="#">Squid</a></li>
+        <li><a href="#">Web Proksi</a></li>
+        <li><a href="#">Ssh 30 Hari</a></li>
+	<li><a href="join/">Join</a></li>
+	<li><a href="#">About</a></li>
+      </ul>
+	<form class="navbar-form navbar-right" role="search">
+      		 <div class="input-group add-on">
+      <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+      <div class="input-group-btn">
+        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+      </div>
+    </div>
+   	 </form>
+    </div>
+  </div>
+</nav>
+<div class="container-fluid">
+<div class="span6" style="float: none; margin:0 auto;">
+  <div class="row">
+	<div class="container-fluid">Tes kontent</div>
+<div class="col-sm-4" data-wow-duration="1000ms" data-wow-delay="600ms"><div class="panel panel-default"><div class="panel-heading"><div class="col-md-4 text-left panel-title"><i class="glyphicon glyphicon-th-large"></i><small><b> SSH Server In Asia</b></small></div></div><div class="panel-body server">Available countries :<br> - Japan<br> - Indonesia<br> - Singapore<br> - Hongkong<br> - Vietnam<br></div><div class="panel-footer"><a href="ssh/?id=1" class="btn btn-default" role="button">Select in Asia</a></div></div></div><div class="col-sm-4" data-wow-duration="1000ms" data-wow-delay="600ms"><div class="panel panel-default"><div class="panel-heading"><div class="col-md-4 text-left panel-title"><i class="glyphicon glyphicon-th-large"></i><small><b> SSH Server In Europe</b></small></div></div><div class="panel-body server">Available countries :<br> - Netherlands<br> - Germany<br> - France<br> - Luxembourg<br> - Russia<br> - United Kingdom<br></div><div class="panel-footer"><a href="ssh/?id=2" class="btn btn-default" role="button">Select in Europe</a></div></div></div><div class="col-sm-4" data-wow-duration="1000ms" data-wow-delay="600ms"><div class="panel panel-default"><div class="panel-heading"><div class="col-md-4 text-left panel-title"><i class="glyphicon glyphicon-th-large"></i><small><b> SSH Server In North America</b></small></div></div><div class="panel-body server">Available countries :<br> - NY, USA<br> - Canada<br> - LA, USA<br></div><div class="panel-footer"><a href="ssh/?id=3" class="btn btn-default" role="button">Select in North America</a></div></div></div><div class="col-sm-4" data-wow-duration="1000ms" data-wow-delay="600ms"><div class="panel panel-default"><div class="panel-heading"><div class="col-md-4 text-left panel-title"><i class="glyphicon glyphicon-th-large"></i><small><b> SSH Server In South America</b></small></div></div><div class="panel-body server">Available countries :<br> - Brazil<br></div><div class="panel-footer"><a href="ssh/?id=4" class="btn btn-default" role="button">Select in South America</a></div></div></div><div class="col-sm-4" data-wow-duration="1000ms" data-wow-delay="600ms"><div class="panel panel-default"><div class="panel-heading"><div class="col-md-4 text-left panel-title"><i class="glyphicon glyphicon-th-large"></i><small><b> SSH Server In Africa</b></small></div></div><div class="panel-body server">Available countries :<br> - Johannesburg<br></div><div class="panel-footer"><a href="ssh/?id=5" class="btn btn-default" role="button">Select in Africa</a></div></div></div><div class="col-sm-4" data-wow-duration="1000ms" data-wow-delay="600ms"><div class="panel panel-default"><div class="panel-heading"><div class="col-md-4 text-left panel-title"><i class="glyphicon glyphicon-th-large"></i><small><b> SSH Server In Oceania</b></small></div></div><div class="panel-body server">Available countries :<br> - Australia<br></div><div class="panel-footer"><a href="ssh/?id=6" class="btn btn-default" role="button">Select in Oceania</a></div></div></div></div></div><br>
+<footer class="container-fluid text-center">
+  <p>© sshcepat.com - 2017</p>
+</footer>
+<div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                        <h3>Company</h3>
+                        <ul>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">We are hiring</a></li>
+                            <li><a href="#">Meet the team</a></li>
+                            <li><a href="#">Copyright</a></li>
+                            <li><a href="#">Terms of use</a></li>
+                            <li><a href="#">Privacy policy</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                </div><!--/.col-md-3-->
+
+                <div class="col-sm-4">
+                        <h3>Support</h3>
+                        <ul>
+                            <li><a href="#">Faq</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Forum</a></li>
+                            <li><a href="#">Documentation</a></li>
+                            <li><a href="#">Refund policy</a></li>
+                            <li><a href="#">Ticket system</a></li>
+                            <li><a href="#">Billing system</a></li>
+                        </ul> 
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-4">
+                        <h3>Developers</h3>
+                        <ul>
+                            <li><a href="#">Web Development</a></li>
+                            <li><a href="#">SEO Marketing</a></li>
+                            <li><a href="#">Theme</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Email Marketing</a></li>
+                            <li><a href="#">Plugin Development</a></li>
+                            <li><a href="#">Article Writing</a></li>
+                        </ul>
+                </div><!--/.col-md-3-->
+
+            </div>
+        </div>
+
+
+</div>
+</body>
+</html>
