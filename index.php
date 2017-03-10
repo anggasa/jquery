69ae5274c704f4255cab29b8f5b2dc3c678f853a
anggasa <anqghasiyaputra@hotmail.com>
2017-03-10 18:10:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <body>&nbsp;<title>Free Premium SSH Account</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="/js/request.js"></script>
+  <link rel="stylesheet" href="https://rawgit.com/anggasa/jquery/master/css/bootstrap.min.css">
+  <script src="https://rawgit.com/anggasa/jquery/master/js/jquery.min.js"></script>
+  <script src="https://rawgit.com/anggasa/jquery/master/js/bootstrap.min.js"></script>
+  <script src="https://rawgit.com/anggasa/jquery/master/js/request.js"></script>
   <style>
 .center_div{
     margin: 0 auto;
